11b64ae0e5c01bf0b62bbb3785ba28bdb5d378a0
havanagila <105500329+sergei-durkin@users.noreply.github.com>
2025-10-01 03:28:18 +0300

    feat: implements insert and move methods for leaf nodes (#5)

--- internal/db/tree.go
+++ internal/db/tree.go
@@ -1,18 +1,26 @@
 package db
 
+import (
+	"fmt"
+)
+
 const (
-	bucketSize    = 1 << 4
-	bucketMinSize = bucketSize / 2
+	maxKeySize = 1 << 10
 )
 
 type Tree struct {
-	root *Page
+	root  *Page
+	pager *Pager
 }
 
-type key []byte
-type entry []byte
+type Key []byte
+type Entry []byte
 
-func (k key) less(other key) bool {
+func (k Key) Valid() bool {
+	return len(k) <= int(maxKeySize)
+}
+
+func (k Key) Less(other Key) bool {
 	if len(k) != len(other) {
 		return len(k) < len(other)
 	}
@@ -24,4 +32,24 @@ func (k key) less(other key) bool {
 	}
 
 	return false
+}
+
+func (t *Tree) find(k Key) (e Entry, found bool) {
+	var err error
+
+	r := t.root
+	if r == nil {
+		r, err = t.pager.ReadRoot()
+		if err != nil {
+			fmt.Println(fmt.Errorf("failed to read root: %w", err))
+
+			return Entry{}, false
+		}
+	}
+
+	if r.IsLeaf() {
+		return r.Leaf().Find(k)
+	}
+
+	return Entry{}, false
 }
